58fb4217fca468950df0554b918ab47b12f1602b
Oorahmaki <nettie.smith@rocketmail.com>
2019-06-01 16:28:48 +0100

    products and checkout js

---
+++ script.js
@@ -0,0 +1,77 @@
+const grid = document.getElementById("grid");
+
+const plants = [
+  {
+    name: "Echeveria",
+    description: "A beginner friendly succulent",
+    image: "images/echeveria.jpg",
+    price: "3.50"
+  },
+  {
+    name: "Sansevieria",
+    description: "A drought tolerant shade lover",
+    image: "images/snakeplant.jpg",
+    price: "7"
+  },
+  {
+    name: "Boston Fern",
+    description: "Perfect for humid dappled shade",
+    image: "images/fern.jpg",
+    price: "5"
+  },
+  {
+    name: "Peace Lily",
+    description: "Shade tolerant and easy to care for",
+    image: "images/peacelily.jpg",
+    price: "5"
+  },
+  {
+    name: "Peperomia",
+    description: "Indirect light and occasional watering",
+    image: "images/peperomia.jpg",
+    price: "3"
+  }
+];
+
+function priceDisplay(price) {
+  //   for (a = 0; a < plants.length; a++) {
+  return "£" + price;
+}
+
+//data = object from array. Contain info needed to create card //
+function createCard(data) {
+  const card = document.createElement("div");
+  const name = document.createElement("h3");
+  const description = document.createElement("p");
+  const image = document.createElement("img");
+  const price = document.createElement("p");
+  const buy = document.createElement("button");
+
+  name.classList.add("name");
+  description.classList.add("description");
+  card.classList.add("cardStyle");
+  image.classList.add("productImage");
+  price.classList.add("price");
+  buy.classList.add("buy");
+
+  name.textContent = data.name;
+  description.textContent = data.description;
+  image.src = data.image;
+  price.textContent = priceDisplay(data.price);
+  buy.textContent = "+";
+
+  card.appendChild(name);
+  card.appendChild(price);
+  card.appendChild(image);
+  card.appendChild(buy);
+  card.appendChild(description);
+  grid.appendChild(card);
+}
+
+function createCards() {
+  for (i = 0; i < plants.length; i++) {
+    createCard(plants[i]);
+  }
+}
+
+createCards();
